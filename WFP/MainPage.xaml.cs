de0b1a9798d94b936c61911e4d0120b08172eb5e
Manh Nam <Manh Nam@DESKTOP-4PD0VRB>
2021-10-18 14:32:53 +0700

    Add project files.

---
+++ WFP/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WFP.Model;
+using System.Collections.ObjectModel;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WFP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<Icons> Icons;
+
+        private ObservableCollection<Contacts> contacts;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Icons = new List<Icons>();
+
+            Icons.Add(new Icons { IconPath = "Assets/image1.jpg" });
+            Icons.Add(new Icons { IconPath = "Assets/image2.jpg" });
+            Icons.Add(new Icons { IconPath = "Assets/image3.jpg" });
+
+            contacts = new ObservableCollection<Contacts>();
+           
+        }
+
+        private void NewContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            string image = ((Icons)ImageComboBox.SelectedValue).IconPath;
+            Contacts.Add(new Contacts { Product = ProductTextBox.Text, Description = DescriptionTextBox.Text, Image = image });
+            ProductTextBox.Text = "";
+            DescriptionTextBox.Text = "";
+            ImageComboBox.SelectedIndex = -1;
+
+
+            ProductTextBox.Focus(FocusState.Programmatic);
+        }
+    }
+}
---
+++ WFP/Model/Contacts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFP.Model
+{
+     public class Contacts
+    {
+        public string Product { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        internal static void Add(Contacts contacts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Icons
+    {
+        public string IconPath { get; set; }
+    }
+}
